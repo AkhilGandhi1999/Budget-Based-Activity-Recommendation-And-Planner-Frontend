125349ace91173e84373c73534f981983df6c80a
JANUSHI SHASTRI <jash6891@colorado.edu>
2023-04-29 00:38:28 -0600

    Added calendar logic for delete event.

--- src/components/PlannerCalendar/PlannerCalendar.js
+++ src/components/PlannerCalendar/PlannerCalendar.js
@@ -1,65 +1,19 @@
 import { defineComponent } from "vue";
-import VueCal from 'vue-cal'
-import 'vue-cal/dist/vuecal.css'
-import QBtn from "quasar";
-import QPopup from "quasar";
+import VueCal from "vue-cal";
+import "vue-cal/dist/vuecal.css";
+
 export default defineComponent({
   name: "PlannerCalendar",
-  components: { QBtn, VueCal },
-  data () {
-    return {
-      title: '',
-      start: '',
-      end: '',
-      events: [],
-      showDialog: false,
-      deletable: false,
-      resizable: false,
-      draggable: false,
-      color: '#0077c2',
-    }
+  components: { VueCal },
+  props: ["events", "height","sDate"],
+  data() {
+    return {};
   },
-
   methods: {
-    showPopup() {
-      this.popupVisible = true
-    },
-
-    openDialog() {
-      this.showDialog = true;
-    },
-    closeDialog() {
-      // reset form data
-      this.title = '';
-      this.start = '';
-      this.end = '';
-      this.showDialog = false;
-    },
-    addEvent(){
-      const newEvent = {
-        title: this.title,
-        start: new Date(this.start),
-        end: new Date(this.end),
-        showDeleteButton: false,
-
-      };
-      this.events.push(newEvent);
-      // reset form data
-      this.title = '';
-      this.start = '';
-      this.end = '';
-      this.showDialog = false;
-
-    },
-    showDeleteButton(event) {
-      // Set showDeleteButton property to true on the clicked event
-      this.events.forEach(evt => {
-        evt.showDeleteButton = evt.id === event.id;
-      });
-    },
-    getEventClass(event) {
-      return event.class;
+    delEvent (emittedEventName, params) {
+      let budgetString = params.content;
+      let budNumber = parseFloat(budgetString.match(/\d+(\.\d+)?/)[0]);
+      this.$emit("removeFromCal", params.title, budNumber);
     },
   }
-
 });
